f93752abc5a0c0a19697ff1edbd3848304d9d55e
zixpykw321 <kirill_de_polyakov@mail.ru>
2025-02-19 23:04:50 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,53 @@
+const coinsElement = document.getElementById("coins");
+
+const resourcesElement = document.getElementById("resources");
+
+const collectButton = document.getElementById("collectButton");
+
+const buildButton = document.getElementById("buildButton");
+
+
+
+let coins = 0;
+
+let resources = 0;
+
+
+
+function updateUI() {
+
+    coinsElement.textContent = coins;
+
+    resourcesElement.textContent = resources;
+
+}
+
+
+
+collectButton.addEventListener("click", () => {
+
+    resources += 1;
+
+    updateUI();
+
+});
+
+
+
+buildButton.addEventListener("click", () => {
+
+    if (resources >= 5) {
+
+        resources -= 5;
+
+        coins += 10;
+
+        updateUI();
+
+    } else {
+
+        alert("Недостаточно ресурсов!");
+
+    }
+
+});
